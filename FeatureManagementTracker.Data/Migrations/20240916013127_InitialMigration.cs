a693344650329db359b6cf3d519608a45b8ba2f7
Ravi Teja Takkellapati <ravit@venuevision.com>
2024-09-15 21:48:16 -0400

    Adding InitialMigration for Features Table.

---
+++ FeatureManagementTracker.Data/Migrations/20240916013127_InitialMigration.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace FeatureManagementTracker.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class InitialMigration : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Features",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Title = table.Column<string>(type: "nvarchar(1000)", maxLength: 1000, nullable: false),
+                    Description = table.Column<string>(type: "nvarchar(max)", maxLength: 5000, nullable: false),
+                    EstimatedComplexity = table.Column<string>(type: "nvarchar(2)", maxLength: 2, nullable: false),
+                    Status = table.Column<string>(type: "nvarchar(10)", maxLength: 10, nullable: false),
+                    TargetCompletionDate = table.Column<DateTime>(type: "datetime2", nullable: true),
+                    ActualCompletionDate = table.Column<DateTime>(type: "datetime2", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Features", x => x.Id);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Feature_Status",
+                table: "Features",
+                column: "Status");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Feature_Title",
+                table: "Features",
+                column: "Title");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Features");
+        }
+    }
+}
